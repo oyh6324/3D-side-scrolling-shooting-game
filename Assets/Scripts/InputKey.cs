e7e49cacbe35d0d49969c921bc69145edbbd876b
oyh6415 <dudgus6324@naver.com>
2021-02-17 16:05:30 +0900

    도감, 이용권
    * 도감 버튼 수정
    * 이용권 자정마다 충전 수정

---
+++ Assets/Scripts/InputKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputKey : MonoBehaviour
+{
+    public string horizontalKeyName;
+    public string fireKeyName;
+    public string jumpKeyName;
+
+    public float horizonAxis { get; private set; }
+    public bool fire { get; private set; }
+    public bool jump { get; private set; }
+
+    private void FixedUpdate()
+    {
+        horizonAxis = Input.GetAxis(horizontalKeyName);
+        jump = Input.GetButtonDown(jumpKeyName);
+        fire = Input.GetButtonDown(fireKeyName);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private InputKey inputKey;
+    private Rigidbody rigidbody;
+
+    public float speed ;
+    public float jumpSpeed;
+    private float currentJumpY;
+    private void Awake()
+    {
+        inputKey = GetComponent<InputKey>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector3(inputKey.horizonAxis * speed, 0f, 0f);
+        if (inputKey.jump) currentJumpY = jumpSpeed;
+        currentJumpY += Time.deltaTime * Physics.gravity.y;
+    }
+}
